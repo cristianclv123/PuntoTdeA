70d0d110842842abd5cc10fa0c0d720b46878c41
Julian082000 <juliansernamax@gmail.com>
2026-09-18 23:00:00 -0500

    Se agrego campo para poder montar imagenes

--- announcements/admin.py
+++ announcements/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Announcement
 
-# Register your models here.
+# Registramos el modelo
+admin.site.register(Announcement)
--- announcements/views.py
+++ announcements/views.py
@@ -1,11 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Announcement
 
 def index(request):
-    # Consultamos todos los anuncios de la base de datos, ordenados del más reciente al más antiguo
+    if request.method == 'POST':
+        # Capturamos los datos de los inputs del HTML
+        title = request.POST.get('title')
+        body_text = request.POST.get('body')  # Viene del name="body" del HTML de tu compañera
+        notice_type = request.POST.get('type') # 'urgente' o 'informativo'
+        image_file = request.FILES.get('image') #esto es para la imagen
+        
+        # Determinamos si es urgente basándonos en lo que seleccionó el usuario
+        is_urgent = True if notice_type == 'urgente' else False
+
+        # Guardamos en la base de datos usando el campo 'content' 
+        Announcement.objects.create(
+            title=title,
+            content=body_text,  # Mapeamos 'body' del HTML al campo 'content' del modelo
+            image=image_file,
+            is_urgent=is_urgent
+        )
+        return redirect('announcements:index')
+
+   
+    #Listado de avisos
     announcements = Announcement.objects.all().order_by('-created_at')
-    
-    # Filtramos específicamente los que son urgentes para cumplir con la HU-08
     urgent_announcements = Announcement.objects.filter(is_urgent=True).order_by('-created_at')
     
     context = {
